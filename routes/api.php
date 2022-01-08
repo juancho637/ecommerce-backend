455334a819503b9cce71c8645e5c1ac11de1f6e8
Juan David Garcia <juancho637@gmail.com>
2022-01-08 18:34:41 -0500

    integración de las rutas para los módulos de países, estados/departamentos y ciudades

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\City\CityController;
+use App\Http\Controllers\Api\State\StateController;
+use App\Http\Controllers\Api\Country\CountryController;
 
+Route::group(['prefix' => 'v1'], function () {
 
-    return $request->user();
+    Route::apiResource('countries', CountryController::class, [
+        'only' => [
+            'index',
+            'store',
+            'show',
+            'update',
+            'destroy',
+        ],
+        'as' => 'api'
+    ]);
+    Route::apiResource('states', StateController::class, [
+        'only' => [
+            'index',
+            'store',
+            'show',
+            'update',
+            'destroy',
+        ],
+        'as' => 'api'
+    ]);
+    Route::apiResource('cities', CityController::class, [
+        'only' => [
+            'index',
+            'store',
+            'show',
+            'update',
+            'destroy',
+        ],
+        'as' => 'api'
+    ]);
 });
