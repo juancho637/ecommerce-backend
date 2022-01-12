60e22cd7a3822f7febaca2ac875a2e108c6623dd
Juan David Garcia <juancho637@gmail.com>
2022-01-12 14:33:16 -0500

    integración de archivos y paquetes para el testing base

---
+++ tests/Feature/Api/ApiTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+
+abstract class ApiTestCase extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
