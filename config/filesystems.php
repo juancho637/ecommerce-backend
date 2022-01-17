137d5610cdeda1d86115e9cd40954f48cf434bef
Juan David Garcia <juancho637@gmail.com>
2022-01-17 14:30:29 -0500

    integración de parametrización para el sistema de archivos

--- config/filesystems.php
+++ config/filesystems.php
@@ -15,6 +15,10 @@ return [
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
 
+    'public' => env('FILESYSTEM_PUBLIC', 'public'),
+
+    'private' => env('FILESYSTEM_PRIVATE', 'local'),
+
     /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
@@ -38,7 +42,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
