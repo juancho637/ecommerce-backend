93e155770bd012bde3390b288834c4afe37ada4e
Juan David Garcia <juancho637@gmail.com>
2022-01-20 14:33:08 -0500

    eliminación de importación que no se utilizaba

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Resources\CategoryResource;
-use App\Transformers\CategoryTransformer;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
