0b6aca6d6ffba3e0396f36102b1ff7582ba6456e
Juan David Garcia <juancho637@gmail.com>
2022-05-25 17:18:55 -0500

    modificación del schema https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_HTTPS', false) === true) {
+            URL::forceScheme('https');
+        }
     }
 }
