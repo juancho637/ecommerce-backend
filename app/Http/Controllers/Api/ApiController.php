e2459488fa7854143e993d2415b38e83c3696759
Juan David Garcia <juancho637@gmail.com>
2022-07-15 19:28:25 -0500

    Creación de la información base para la documentación

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -8,4 +8,18 @@ use App\Http\Controllers\Controller;
 class ApiController extends Controller
 {
     use ApiResponse;
+
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="E-commerce API Documentation",
+     * ),
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST
+     * )
+     */
+    public function __construct()
+    {
+        //
+    }
 }
