93cfda920e6b9aeb8c3ad537c018e8495b6c7359
Juan David Garcia <juancho637@gmail.com>
2022-10-21 19:07:22 -0500

    update documentation to update category request

--- app/Http/Requests/Api/Category/UpdateCategoryRequest.php
+++ app/Http/Requests/Api/Category/UpdateCategoryRequest.php
@@ -21,9 +21,9 @@ class UpdateCategoryRequest extends FormRequest
     }
 
     /**
-     * @OA\Property(type="string", description="name", property="name", nullable=true),
-     * @OA\Property(type="file", description="image", property="image", nullable=true),
-     * @OA\Property(type="number", description="category parent id assigned", property="parent_id", nullable=true),
+     * @OA\Property(type="string", description="name", property="name"),
+     * @OA\Property(type="string", format="binary", description="image", property="image"),
+     * @OA\Property(type="number", description="category parent id assigned", property="parent_id"),
      */
     public function rules()
     {
