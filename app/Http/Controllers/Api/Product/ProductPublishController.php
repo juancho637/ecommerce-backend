74a71fe5962b496b48a43833e934b0e565c4b0fe
Juan David Garcia <>
2023-03-15 00:28:02 -0500

    init publish product

---
+++ app/Http/Controllers/Api/Product/ProductPublishController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Product;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
+
+class ProductPublishController extends ApiController
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
