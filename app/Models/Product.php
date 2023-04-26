aabb2b7acdebdce5e6f5ec28d8c91fed0ae11682
Juan David Garcia <>
2023-04-26 16:58:36 -0500

    add validation to restore product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -347,6 +347,11 @@ class Product extends Model
     {
         if ($this->status_id === Status::disabled()->value('id')) {
             $this->status_id = Status::enabled()->value('id');
+            if ($this->is_variable) {
+                $this->status_id = Status::productStocksStep()->value('id');
+            } else {
+                $this->status_id = Status::productGeneralStep()->value('id');
+            }
         } else if ($this->status_id === Status::enabled()->value('id')) {
             $this->status_id = Status::disabled()->value('id');
         }
